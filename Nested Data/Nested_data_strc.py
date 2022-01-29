64a48e24d4f9bfbd35e909f3b882265c3f1b2d1a
elxinoconequis <jf.ortega.silva@gmail.com>
2022-01-29 13:45:03 -0600

    Nested Data lesson

---
+++ Nested Data/Nested_data_strc.py
@@ -0,0 +1,27 @@
+# Nested List
+
+info = {'personal_data':
+         {'name': 'Lauren',
+          'age': 20,
+          'major': 'Information Science',
+          'physical_features':
+             {'color': {'eye': 'blue',
+                        'hair': 'brown'},
+              'height': "5'8"}
+         },
+       'other':
+         {'favorite_colors': ['purple', 'green', 'blue'],
+          'interested_in': ['social media', 'intellectual property', 'copyright', 'music', 'books']
+         }
+      }
+color=info['personal_data']['physical_features']['color']
+
+# Nested Iteration
+# Deep copy:
+# Shallow copy:
+
+
+
+
+
+
--- README.txt
+++ README.txt
@@ -1,1 +1,3 @@
 Curso empezado el 29 de enero de 2022
+
+Nota: Debo empeza a practicar más con los diccionarios. En retrospectiva, pude facilitar el "Sentiment Classifier" con los diccionarios.
